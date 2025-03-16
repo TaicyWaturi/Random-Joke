44749a2e7aa3df8ffe4acbfba0e9ae5de35e7649
TaicyWaturi <taicywaturi@gmail.com>
2025-03-17 02:47:43 +0300

    Added joke.py

---
+++ joke.py
@@ -0,0 +1,10 @@
+import random
+
+# Random Joke Generator
+jokes = [
+    "Why do programmers prefer dark mode? Because light attracts bugs! 🐛",
+    "Why was the JavaScript developer sad? Because he didn't 'null' his problems. 😢",
+    "Why do Python developers prefer snakes? Because they love 'pythonic' code! 🐍"
+]
+
+print(random.choice(jokes))
